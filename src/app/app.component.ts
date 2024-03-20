41a8a81dc38b234340099a411817398ee8136a68
Manu Bharadwaj BN <102406248+Manu577228@users.noreply.github.com>
2024-03-20 19:13:29 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'calculator';
+  result = '';
+
+  handleInput(input: string) {
+    if (input === 'C') {
+      this.result = '';
+    } else {
+      this.result += input;
+    }
+  }
+
+  calculate() {
+    try {
+      this.result = eval(this.result);
+    } catch (e) {
+      this.result = 'Error';
+    }
+  }
 }
